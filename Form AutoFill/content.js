cfdcc0e11cdf04e3713add43869ef513f5be4c91
Paul Jacob.v <pauljacobv@Airport19.local>
2017-01-21 22:05:38 +0530

    ChromeExtensions | Repo Cleanup

---
+++ Form AutoFill/content.js
@@ -0,0 +1,12 @@
+// content.js
+
+console.log("Extension works fine!");
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if (request.message === "clicked_browser_action") {
+            var firstHref = $("a[href^='http']").eq(0).attr("href");
+            console.log(firstHref);
+        }
+    }
+);
---
+++ Form AutoFill/login.js
@@ -0,0 +1,7 @@
+// login.js
+
+$(document).ready(function () {
+    $("#fName").val("username");
+    $("#lName").val("password");
+    $('#submit').click()
+});
